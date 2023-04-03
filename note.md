10fc53f04b3dfecec653b7d50ff6c1b3ab1c3d37
Juhani777 <1914250258@qq.com>
2023-04-03 21:40:29 +0800

    1-3 整合swagger

--- note.md
+++ note.md
@@ -1,4 +1,7 @@
 ### 一.swagger
 - 1.生成在线接口文档
 - 2.方便接口测试
+- swagger固定访问地址：
+  - swagger-ui.html
+
 
--- service/service_edu/src/main/java/com/Juhani/eduService/EduApplication.java
+++ service/service_edu/src/main/java/com/Juhani/eduService/EduApplication.java
@@ -3,9 +3,11 @@ package com.Juhani.eduService;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 //@MapperScan("com.Juhani.eduService.mapper")
+@ComponentScan(basePackages = {"com.Juhani"})
 public class EduApplication {
     public static void main(String[] args) {
         SpringApplication.run(EduApplication.class,args);
